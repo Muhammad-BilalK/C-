8b06eb0bfb6e54478523454be201b9248dc6e950
Muhammad Bilal Khan <121548538+Muhammad-BilalK@users.noreply.github.com>
2023-01-06 18:45:04 +0500

    Add files via upload

---
+++ c2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //int a = 23
+    //int b = 6
+    int a=23, b=16;
+    float pi= 3.14;
+    char c='d';
+    cout<<"The values are. \nValue of a is "<<a<<". \nValue of b is "<<b;
+    cout<< "\nValue of pi is: "<<pi;
+    cout<<"\nValue of c is: "<<c;
+    return 0;
+}
